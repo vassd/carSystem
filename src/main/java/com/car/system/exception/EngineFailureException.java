5242789adfded7a0c43d03c14489d722540dd789
David Vass <nighturbex@protonmail.com>
2024-10-15 15:19:48 +0200

    add exceptions

---
+++ src/main/java/com/car/system/exception/EngineFailureException.java
@@ -0,0 +1,7 @@
+package com.car.system.exception;
+
+public class EngineFailureException extends Exception {
+    public EngineFailureException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/car/system/exception/FuelEmptyException.java
@@ -0,0 +1,7 @@
+package com.car.system.exception;
+
+public class FuelEmptyException extends Exception {
+    public FuelEmptyException(String message) {
+        super(message);
+    }
+}
